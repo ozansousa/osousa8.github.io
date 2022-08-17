e5d1fe467aa3edcd204fbae9be5548598b8101e7
Osousa8 <Osousa8@users.noreply.github.com>
2022-08-17 19:31:07 +0000

    deploy: b8831cfc97a10fc237568789428e8514347de25c

--- indexscript.js
+++ indexscript.js
@@ -1,19 +1,22 @@
-let img = 0
+let img = 0;
 function Change(){
     if (img ==0){
-      document.getElementById("img").setAttribute("src", "/creditosaude.png");
+      document.getElementById("img").setAttribute("src", "/mainimagecasal.png");
     }
     else if (img ==1){
-      document.getElementById("img").setAttribute("src", "/creditoinformatica.png");
-    }
-    else if (img ==2){
       document.getElementById("img").setAttribute("src", "/microcredito.png");
     }
-    else if(img ==3){
+    else if (img ==2){
       document.getElementById("img").setAttribute("src", "/creditopessoal.png");
-      img=0;
+    }
+    else if(img ==3){
+      document.getElementById("img").setAttribute("src", "/creditoinformatica.png");
+    }
+    else if(img ==4){
+      document.getElementById("img").setAttribute("src", "/creditosaude.png");
+      img=-1;
       return;
     }
     img++;
 }
-  setInterval(Change, 2000);
+setInterval(Change, 2000);
